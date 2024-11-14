b54595e8f1f36862619a6ea9b4ad5a8d29201316
aras123-as <mehmeterlap@gmail.com>
2024-11-14 16:07:51 +0300

    Update README.py

--- README.py
+++ README.py
@@ -1,1 +1,12 @@
-# 
+# iki nokta arsındaki uzaklık formülünü koda dökme. 
+x1=int(intup("lütfen x1 deyerini gir "))
+x2=int(intup("lütfen x2 deyerini gir "))
+y1=int(intup("lütfen y1 deyerini gir "))
+y2=int(intup("lütfen y2 deyerini gir "))
+
+formül=((x1-x2)**2+(y1-y2)**2)**0.5
+print("kare kök içerisinde {}-{}'nin karesi+{}+{}'nin karesi".format(x1,x2,y1,y2,formül)
+
+
+
+
